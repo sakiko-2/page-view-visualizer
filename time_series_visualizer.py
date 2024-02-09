54149332936fc5facf6c1e6676bf47c1c76ba3e6
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-02-09 23:45:00 +1300

    Bar plot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,4 +1,6 @@
+import calendar
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
@@ -20,19 +22,29 @@ def draw_line_plot():
     fig.savefig('line_plot.png')
     return fig
 
+
 def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
+    df_bar = df_bar.groupby([df.index.year, df.index.month]).mean().unstack()
 
-    # Draw bar plot
+    fig, ax = plt.subplots(figsize=(8, 7))
+    width = 0.05
+    multiplier = 0
 
+    for month, y in df_bar.items():
+        offset = width * multiplier
+        x = np.arange(len(df_bar.index))
+        ax.bar(x + offset, y, width, label=calendar.month_name[month[1]])
+        multiplier += 1
 
+    ax.set_xlabel('Years')
+    ax.set_ylabel('Average Page Views')
+    ax.set_xticks(x + width * len(df_bar.keys()) / 2 - 0.025, df_bar.index, rotation='vertical')
+    ax.legend(loc='upper left', title='Months')
 
-
-
-    # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
